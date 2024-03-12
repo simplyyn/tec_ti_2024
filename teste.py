a9d6086b1fa716887cc46305c68882635d82bd3c
simplyyn <107005583+simplyyn@users.noreply.github.com>
2024-03-12 08:48:57 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -5,11 +5,11 @@ opcao = int(input('[1] para cadastrar um novo produto\n[2] para alterar os dados
 if opcao == 1:
     nome_do_produto = str(input('Digite o nome do pruduto: '))
     cod_do_produto = str(input('digite o codigo do produto: '))
-    preco_de_venda = str(input('digite o preço de venda do produto:'))
-    custo_do_produto = str(input('digite o custo do produto: : '))
-    custo_fixo = str(input('digite o custo fixo do produto: '))
-    comissao_de_venda = str(input('qual a comissão de venda'))
-    imposto_sobre_venda = str(input('digite o imposto sobre venda: '))
+    preco_de_venda = float(input('digite o preço de venda do produto:'))
+    custo_do_produto = float(input('digite o custo do produto: : '))
+    custo_fixo = int(input('digite o custo fixo do produto: '))
+    comissao_de_venda = int(input('qual a comissão de venda'))
+    imposto_sobre_venda = int(input('digite o imposto sobre venda: '))
 
 elif opcao == 2:
     nome_do_produto = str(input('Digite o nome do pruduto: '))
