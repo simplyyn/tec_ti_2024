7d5458464d1ac1a1d203f38ecea4ebc7f0251d5a
simplyyn <107005583+simplyyn@users.noreply.github.com>
2024-03-15 09:22:04 -0300

    Update teste.py
    atualização do cod

--- teste.py
+++ teste.py
@@ -1,16 +1,39 @@
 print('#'*20)
-print('menu de opçoes')
+print(' '*3,'menu de opçoes')
 print('#'*20)
-opcao = int(input('[1] para cadastrar um novo produto\n[2] para alterar os dados de algum produto\n[3] para listar os produtos'))
-if opcao == 1:
-    nome_do_produto = str(input('Digite o nome do pruduto: '))
-    cod_do_produto = str(input('digite o codigo do produto: '))
-    preco_de_venda = float(input('digite o preço de venda do produto:'))
-    custo_do_produto = float(input('digite o custo do produto: : '))
-    custo_fixo = int(input('digite o custo fixo do produto: '))
-    comissao_de_venda = int(input('qual a comissão de venda'))
-    imposto_sobre_venda = int(input('digite o imposto sobre venda: '))
+while True:
+    opcao = int(input('[1] para cadastrar um novo produto\n[2] para alterar os dados de algum produto\n[3] para listar os produtos\nDigite aqui: '))
+    if opcao == 1:
+        nome_do_produto = str(input('Digite o nome do pruduto: '))
+        cod_do_produto = str(input('digite o codigo do produto: '))
+        preco_de_venda = float(input('digite o preço de venda do produto:'))
+        custo_do_produto = float(input('digite o custo do produto: : '))
+        custo_fixo = int(input('digite o custo fixo do produto: '))
+        comissao_de_venda = int(input('qual a comissão de venda: '))
+        imposto_sobre_venda = int(input('digite o imposto sobre venda: '))
+        confirmacao = str(input("produto cadastrado. deseja voltar para o menu? S/N: "))
+        if confirmacao == "s" or "S":
+            continue
+        elif confirmacao == "n" or "N":
+            break
 
-elif opcao == 2:
-    nome_do_produto = str(input('Digite o nome do pruduto: '))
-elif opcao
+    elif opcao == 2:
+        nome_do_produto = str(input('Digite o nome do pruduto: '))
+        cod_do_produto = str(input('digite o codigo do produto: '))
+        preco_de_venda = float(input('digite o preço de venda do produto:'))
+        custo_do_produto = float(input('digite o custo do produto: : '))
+        custo_fixo = int(input('digite o custo fixo do produto: '))
+        comissao_de_venda = int(input('qual a comissão de venda'))
+        imposto_sobre_venda = int(input('digite o imposto sobre venda: '))
+        confirmacao = str(input("produto cadastrado. deseja voltar para o menu? S/N: "))
+        if confirmacao == "s" or "S":
+            continue
+        elif confirmacao == "n" or "N":
+            break
+    elif opcao == 3:
+        print(f"Nome do produto: {nome_do_produto}\nCodigo do produto: {cod_do_produto}")
+        confirmacao = str(input("deseja voltar para o menu? S/N: "))
+        if confirmacao == "s" or "S":
+            continue
+        elif confirmacao == "n" or "N":
+            break
