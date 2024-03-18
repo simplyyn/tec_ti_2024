0bdf999be0856ddca9c65b518c8ed569484fe89d
simplyyn <107005583+simplyyn@users.noreply.github.com>
2024-03-18 17:53:23 -0300

    Update teste.py
    adicionei se teve lucro ou não

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
-print('#'*20)
-print(' '*3,'menu de opçoes')
-print('#'*20)
+print(' '*7,'='*40)
+print(' '*11,'PROGRAMA PARA CADASTRAR PRODUTOS')
+print(' '*7,'='*40)
 while True:
     opcao = int(input('[1] para cadastrar um novo produto\n[2] para alterar os dados de algum produto\n[3] para listar os produtos\nDigite aqui: '))
     if opcao == 1:
@@ -13,8 +13,23 @@ while True:
         margem_de_lucro = float(input('\ndigite a margem de lucro em %: '))
         preco_de_vendap1 = (((custo_fixo)/100) + ((comissao_de_venda)/100) + ((imposto_sobre_venda)/100) + ((margem_de_lucro)/100)/(100/100))
         preco_de_venda_final = custo_do_produto / (1 - preco_de_vendap1)
-        print(f"O preço de venda do seu roduto será de: R${float(preco_de_venda_final):.2f}")
+        print(f"O preço de venda do seu produto será de: R${float(preco_de_venda_final):.2f}")
+        margem_de_lucro = custo_do_produto/preco_de_venda_final
+        lucro = margem_de_lucro * 100
+        if lucro > 20:
+            print(f'lucro alto: %{lucro} de lucro')
+        if lucro > 10 :
+            print(f'lucro médio: %{lucro} de lucro')
+        if lucro > 0:
+            print(f'lucro baixo: %{lucro} de lucro')
+        if lucro == 0:
+            print(f'equilíbrio: %{lucro} de lucro')
+        if lucro < 0:
+            print(f'prejuizo %{lucro} de lucro')
+                
         confirmacao = str(input("produto cadastrado. deseja voltar para o menu? S/N: "))
+        
+        
         if confirmacao == "s" or "S":
             continue
         elif confirmacao == "n" or "N":
